617f08655bde602c2e5789f429adf00e38f82188
MariSlvrstv <124884503+MariSlvrstv@users.noreply.github.com>
2023-09-21 21:49:52 +0300

    add main func

---
+++ ByteCoin/Controllers/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  ByteCoin
+//
+//  Created by Мария Селиверстова on 21.09.2023.
+//
+
+import Foundation
---
+++ ByteCoin/Models/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Мария Селиверстова on 21.09.2023.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    
+    let rate: Double
+}
